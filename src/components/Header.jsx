b0ceef00066f180c0b71f5110cd75b1aceae56c1
Anton Kizerev <70804175+kosmodromm@users.noreply.github.com>
2021-03-29 23:27:27 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://kupipet.ru/upload/medialibrary/4c3/4c336f7a5acfe8ee6a9e98aa49e26631.jpg" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://avatars.mds.yandex.net/get-zen_doc/1875939/pub_5fe9c4e2dba1eb4af8f51499_5fe9c7603ede826fd4e820a7/scale_1200"></img>
+      </div>
+      <div className="avatar">ava +descr</div>
+      <div className="mypost">
+        mypost
+        <div>
+          newpost
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
